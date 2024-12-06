bbc786c58482389655fb388f0c79a24048a05310
exxxar <exxxar@gmail.com>
2024-12-07 02:20:59 +0300

    pdate

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,7 +21,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
-        DB::statement("SET GLOBAL max_connections = 200");
+      //      DB::statement("SET GLOBAL max_connections = 200");
 
     }
 }
